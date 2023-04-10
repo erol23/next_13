35f19b5a795f4aa55897965b4c971fd207b8e915
erol23 <hurkanrol15@gmail.com>
2023-04-10 12:10:33 +0200

    fetch repos data

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import Header from "./components/Header";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={poppins.className}>
+        <Header />
         <main className="container">{children}</main>
       </body>
     </html>
